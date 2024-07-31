d95c3d219a14f2b8dbe3aacf83366e1685324989
carlosvn <carlos.vneves@gmail.com>
2024-07-30 21:32:00 -0300

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -151,10 +151,10 @@ fn tokenize(lexeme: &str) -> Result<(), i32>{
                 if lexeme.chars().count() > 1{
                     if let Some('/') = chars.peek().cloned() {
                         
-                        while chars.next() != Some('\n'){
-                            chars.next(); 
-                        }
-                        // break;
+                        // while chars.next() != Some('\n'){
+                        //     chars.next(); 
+                        // }
+                        break;
 
                     } else {
                         println!("{}", Token::Slash);
