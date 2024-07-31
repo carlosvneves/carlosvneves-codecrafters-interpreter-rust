06baeb836b5ea3876187728b5417d74fc405aaf8
= <=>
2024-07-31 09:52:43 -0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,14 @@ use std::io::{self, Write};
 use std::process;
 
 fn main() {
-    
-
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         writeln!(io::stderr(), "Usage: {} tokenize <filename>", args[0]).unwrap();
-        return ;     
+        return;
     }
 
     let command = &args[1];
     let filename = &args[2];
-
-    
 
     match command.as_str() {
         "tokenize" => {
@@ -29,20 +25,20 @@ fn main() {
                 String::new()
             });
 
-            let _ =tokenize(&file_contents);        
+            let _ = tokenize(&file_contents);
         }
         _ => {
             writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
-            return ;
+            return;
         }
     }
 }
 
 // <token_type> <lexeme> <literal>
 //, , ., -, +, ;, *. /
-enum Token{
+enum Token {
     EOF,
-    RightParen,     
+    RightParen,
     LeftParen,
     RightBrace,
     LeftBrace,
@@ -53,7 +49,6 @@ enum Token{
     Semicolon,
     Star,
     Slash,
-
 
     // Dot,
     // Comma,
@@ -94,144 +89,131 @@ impl fmt::Display for Token {
             Token::EqualEqual => write!(f, "EQUAL_EQUAL == null"),
             Token::Bang => write!(f, "BANG ! null"),
             Token::BangEqual => write!(f, "BANG_EQUAL != null"),
-            Token::Less=> write!(f, "LESS < null"),
-            Token::LessEqual=>write!(f, "LESS_EQUAL <= null"),
+            Token::Less => write!(f, "LESS < null"),
+            Token::LessEqual => write!(f, "LESS_EQUAL <= null"),
             Token::Greater => write!(f, "GREATER > null"),
-            Token::GreaterEqual =>write!(f, "GREATER_EQUAL >= null"),
-
+            Token::GreaterEqual => write!(f, "GREATER_EQUAL >= null"),
         }
     }
 }
 
-
-fn tokenize(lexeme: &str) -> Result<(), i32>{
-
-
-    // TODO: 
+fn tokenize(lexeme: &str) -> Result<(), i32> {
+    // TODO:
     // implementar vector para fazer push dos tokens
     // refatorar código - DRY para a avaliação de ops compostos com '='
-    let mut status:i32 = 0;
+    let mut status: i32 = 0;
     let mut line = 1;
     let mut chars = lexeme.chars().peekable();
+    let lexeme_size = lexeme.chars().count();
+
     while let Some(f) = chars.next() {
         match f {
             '(' => {
-                println!("{}",Token::LeftParen);
-            },
+                println!("{}", Token::LeftParen);
+            }
             ')' => {
-                println!("{}",Token::RightParen);
-            },
+                println!("{}", Token::RightParen);
+            }
             '{' => {
                 println!("{}", Token::LeftBrace);
-            },
-            '}' =>{
+            }
+            '}' => {
                 println!("{}", Token::RightBrace);
-
-            },
+            }
             ',' => {
                 println!("{}", Token::Comma);
-            },
-            '.' =>{
+            }
+            '.' => {
                 println!("{}", Token::Dot);
-            },
-            '-' =>{
+            }
+            '-' => {
                 println!("{}", Token::Minus);
-            },
-            '+' =>{
+            }
+            '+' => {
                 println!("{}", Token::Plus);
-            },
-            ';' =>{
+            }
+            ';' => {
                 println!("{}", Token::Semicolon);
-            },
-            '*' =>{
+            }
+            '*' => {
                 println!("{}", Token::Star);
-            },
-            '/' =>{
-                if lexeme.chars().count() > 1{
+            }
+            '/' => {
+                if lexeme.chars().count() > 1 {
                     if let Some('/') = chars.peek().cloned() {
-                        
-                        while chars.next() != Some('\n'){
-                            chars.next(); 
+                        let mut pos = 2;
+                        while chars.peek().cloned() != Some('\n') && pos <= lexeme_size {
+                            chars.next();
+                            pos += 1;
                         }
-                        // break;
-
                     } else {
                         println!("{}", Token::Slash);
                     }
-                  } else {
+                } else {
                     println!("{}", Token::Slash);
                 }
-            },
+            }
             '=' => {
-                if lexeme.chars().count() > 1{
+                if lexeme.chars().count() > 1 {
                     if let Some('=') = chars.peek().cloned() {
-                            chars.next(); // Consume the '='
+                        chars.next(); // Consume the '='
                         println!("{}", Token::EqualEqual);
                     } else {
                         println!("{}", Token::Equal);
                     }
-                  } else {
+                } else {
                     println!("{}", Token::Equal);
                 }
-                
-            },
-            '!'=>{
-                if lexeme.chars().count() > 1{
+            }
+            '!' => {
+                if lexeme.chars().count() > 1 {
                     if let Some('=') = chars.peek().cloned() {
-                        chars.next();                         
+                        chars.next();
                         println!("{}", Token::BangEqual);
                     } else {
                         println!("{}", Token::Bang);
                     }
-                  } else {
+                } else {
                     println!("{}", Token::Bang);
                 }
-            },
-            '>'=>{
-
-                if lexeme.chars().count() > 1{
+            }
+            '>' => {
+                if lexeme.chars().count() > 1 {
                     if let Some('=') = chars.peek().cloned() {
-                        chars.next();                         
+                        chars.next();
                         println!("{}", Token::GreaterEqual);
                     } else {
                         println!("{}", Token::Greater);
                     }
-                  } else {
+                } else {
                     println!("{}", Token::Greater);
                 }
-            },
-            '<'=>{
-                if lexeme.chars().count() > 1{
+            }
+            '<' => {
+                if lexeme.chars().count() > 1 {
                     if let Some('=') = chars.peek().cloned() {
-                        chars.next();                         
+                        chars.next();
                         println!("{}", Token::LessEqual);
                     } else {
                         println!("{}", Token::Less);
                     }
-                  } else {
+                } else {
                     println!("{}", Token::Less);
                 }
-
-            },
-            ' '=>{
-
-
-            },
-            '\t'=>{
-
-            },
-            '\n'=>{
-                line+=1;
-
-            },
-            _ =>{
-                eprintln!("[line {}] Error: Unexpected character: {}",line, f);
+            }
+            '\r' => {}
+            ' ' => {}
+            '\t' => {}
+            '\n' => {
+                line += 1;
+            }
+            _ => {
+                eprintln!("[line {}] Error: Unexpected character: {}", line, f);
                 status = 65;
-                
             }
         };
     }
-    println!("{}",Token::EOF);
+    println!("{}", Token::EOF);
 
     process::exit(status);
 }
@@ -240,4 +222,8 @@ fn tokenize(lexeme: &str) -> Result<(), i32>{
 //
 //     retun format!("{}")
 //
-// } 
+// }
+
+// fn is_at_end(size: i32, pos: i32) -> bool {
+//     return true;
+// }
